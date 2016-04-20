e9ec5153249f86db6d795e324862d431feb167b8
MichaelCraddock <MichaelGCraddock@hotmail.co.uk>
2016-04-20 17:04:51 +0100

    Deformations working to a very simple level, Swaps out between models and also has a health reset button bound to R.

--- Assets/Scripts/Deform.cs
+++ Assets/Scripts/Deform.cs
@@ -6,6 +6,11 @@ public class Deform : MonoBehaviour
     private Material material;
     public CarController car;
     public AudioSource crashAudio;
+    public GameObject CarshellN, CarshellB, CarshellVB, CarshellW;
+    public float carhealth = 100;
+    public float carbumperhealth = 100;
+    public Texture Normal;
+    public Texture Damaged;
 
     private float destructionLevel = 0.0f;
 
@@ -29,7 +34,52 @@ public class Deform : MonoBehaviour
         //    material.SetFloat("_Displacement", destructionLevel);
         //    crashAudio.Play();
         //}
+        if (carhealth <= 100)
+        {
+            CarshellN.SetActive(true);
+            CarshellB.SetActive(false);
+            CarshellVB.SetActive(false);
+        }
 
+        if (carhealth <= 75)
+        {
+            Debug.Log("Health is at 75%");
+            CarshellN.SetActive(false);
+            CarshellB.SetActive(true);
+            CarshellVB.SetActive(false);
+        }
 
+        if (carhealth <= 50)
+        {
+            Debug.Log("Health is at 50%");
+            CarshellB.SetActive(false);
+            CarshellVB.SetActive(true);
+            CarshellN.SetActive(false);
+        }
+        
+
+        if (carhealth <= 25)
+        {
+            Debug.Log("health is at 25%");
+
+            }
+        
+        if (Input.GetKey(KeyCode.R))
+        {
+            Debug.Log("100 Health");
+            carhealth = 100;
+        }
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enviroment")
+        {
+            Debug.Log("something hit");
+            carhealth -= 5;
+            
+        }
+       
     }
 }
